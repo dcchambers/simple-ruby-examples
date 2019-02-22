e4226f039ff46fae11f0e09341581f7ef95b25cf
Dakota Chambers <dakotachambers@gmail.com>
2019-02-22 12:42:06 -0600

    Add comments to explain each loop code

--- loops/each-loop.rb
+++ loops/each-loop.rb
@@ -1,8 +1,8 @@
 # An example of ruby's each method.
 
 # An array (a collection of objects)
-
 fruits = ["apple", "banana", "orange", "kiwi", "pear", "lime", "grapefruit",
           "lemon", "strawberry", "blueberry", "cranberry", "peach"]
 
+# For each item in the collection...print a statement.
 fruits.each { |fruit| puts "#{fruit} is a tasty fruit!" }
