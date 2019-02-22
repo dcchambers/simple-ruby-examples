02224907283f27580de278aa3a0eb9031eb67077
Dakota Chambers <dakotachambers@gmail.com>
2019-02-22 12:39:58 -0600

    Add a loop example (using ruby's "each" method)
    Probably need to explain arrays also.

---
+++ loops/each-loop.rb
@@ -0,0 +1,8 @@
+# An example of ruby's each method.
+
+# An array (a collection of objects)
+
+fruits = ["apple", "banana", "orange", "kiwi", "pear", "lime", "grapefruit",
+          "lemon", "strawberry", "blueberry", "cranberry", "peach"]
+
+fruits.each { |fruit| puts "#{fruit} is a tasty fruit!" }
